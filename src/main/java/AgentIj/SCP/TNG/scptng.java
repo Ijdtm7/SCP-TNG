b7a09b0080d67bc27398afc3282824870d9cb8e8
Ijdtm7 <48657001+Ijdtm7@users.noreply.github.com>
2019-12-29 12:36:51 -0600

    start mod
    YIPPEE BOIS ITS TIME

---
+++ src/main/java/AgentIj/SCP/TNG/scptng.java
@@ -0,0 +1,11 @@
+package AgentIj.SCP.TNG;
+
+// Created 12/27/19 by Agent Ij
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = scptng.ID,name = scptng.NAME,version = scptng.VERSION,acceptedMinecraftVersions = "[1.12]")
+public class scptng {
+    public static final String ID="scptng", NAME="SCP: The Next Generation", VERSION="0.0.1A";
+}
+
