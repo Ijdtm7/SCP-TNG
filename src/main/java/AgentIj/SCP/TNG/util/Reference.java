9675910f18267111fab04115a4ae86a2c6b8cc1e
BuildTools <unconfigured@null.spigotmc.org>
2019-12-30 21:56:18 -0600

    readding src

--- src/main/java/AgentIj/SCP/TNG/util/Reference.java
+++
@@ -1,9 +0,0 @@
-package AgentIj.SCP.TNG.util;
-
-public class Reference {
-
-    public static final String ID="scptng", NAME="SCP: The Next Generation", VERSION="0.0.1A", CLIENT="AgentIj.SCP.TNG.proxy.ClientProxy", SERVER="AgentIj.SCP.TNG.proxy.CommonProxy";
-
-
-
-}
